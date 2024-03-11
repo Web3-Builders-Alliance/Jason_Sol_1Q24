bd542739464ffef022f2c82c841b3f2533484cbd
Jasondevweb3 <159626090+Jasondevweb3@users.noreply.github.com>
2024-03-11 15:11:27 +0100

    AMM updated

--- Lectures/amm/programs/amm/src/lib.rs
+++ Lectures/amm/programs/amm/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod constants;
+pub mod helpers;
 pub mod error;
 pub mod instructions;
 pub mod state;
@@ -15,7 +16,44 @@ declare_id!("EWx9TC7jrxX2BWLxrHrTYTzchuwcopFG98zAUohZqQUP");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(
+        ctx: Context<Initialize>, 
+        seed: u64, 
+        fee:u16, 
+        authority: Option<Pubkey>
+    ) -> Result<()> {
+        initialize::handler(ctx, seed, fee, authority)
     }
+
+    pub fn deposit(
+        ctx: Context<Deposit>, 
+        amount: u64, // Amount of LP token to claim
+        max_x: u64,  // Max amount of X we are willing to deposit
+        max_y: u64,  // Max amount of Y we are willing to deposit
+        expiration: i64,
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y, expiration)
+    }
+
+    pub fn withdraw(
+        ctx: Context<Withdraw>, 
+        amount: u64, // Amount of LP token to claim
+        min_x: u64,  // Max amount of X we are willing to deposit
+        min_y: u64,  // Max amount of Y we are willing to deposit
+        expiration: i64,
+    ) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y, expiration)
+    }
+
+    pub fn swap(
+        ctx: Context<Swap>, 
+        is_x: bool,
+        amount: u64,  
+        min: u64, 
+        expiration: i64,
+    ) -> Result<()> {
+        ctx.accounts.swap(is_x, amount, min, expiration)
+    }
+
+
 }
---
+++ Lectures/amm/programs/amm/src/state/mod.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct Config {
+    pub seed: u64,
+    pub authority: Option<Pubkey>,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
+    pub fee: u16, // basis point 10000 = 100%
+    pub locked: bool,
+    pub auth_bump: u8,
+    pub config_bump: u8,
+    pub lp_bump: u8,
+}
+
+impl Space for Config {
+    const INIT_SPACE: usize = 8 + (1 + 32) + 32 + 32 + 2 + 1 + 1 + 1 + 1;
+}
