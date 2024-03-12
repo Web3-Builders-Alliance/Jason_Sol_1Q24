03a27c81bfa96b5711a077015d66db8565a0a9ac
Jasondevweb3 <159626090+Jasondevweb3@users.noreply.github.com>
2024-03-12 15:26:57 +0100

    InitializeMarketplace

---
+++ programs/marketplace/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("7848DR2EiaGJrLKLRc5ngtw1MJGQYeV92pLczqtbtfV1");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
